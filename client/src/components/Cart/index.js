6ba85f580bc108f8ef695e87c52877ba1e3fddfe
AlexDeLarge <jmrendon48@gmail.com>
2021-05-19 17:04:23 -0700

    installed react-redux

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -3,7 +3,6 @@ import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
 import "./style.css";
 
-import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 
 import { idbPromise } from "../../utils/helpers";
@@ -12,6 +11,8 @@ import { QUERY_CHECKOUT } from "../../utils/queries";
 import { loadStripe } from "@stripe/stripe-js";
 
 import { useLazyQuery } from "@apollo/react-hooks";
+
+import { Provider } from 
 
 const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
